f1fc5e542f7371442585d0a06b97155981b85cd0
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-02-12 16:40:50 -0700

    centered text to look better on mobile

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -7,8 +7,8 @@ export default function Heading({}: Props) {
     <div className='relative'>
       <div className="absolute inset-0 bg-neon-white rounded-lg blur"></div>
       <div className='relative flex flex-col items-center p-10 md:p-20 border-4 rounded-lg border-solid border-neon-white animate-lights motion-safe:animate-lights bg-deep-blue'>
-        <h1 className='text-6xl md:text-8xl font-neon text-neon-white py-4'>Ashley Morgan</h1>
-        <h3 className='animate-pulse text-2xl md:text-4xl font-subheading text-neon-white py-2'>Portfolio Coming Soon</h3>
+        <h1 className='text-6xl md:text-8xl font-neon text-neon-white py-4 text-center'>Ashley Morgan</h1>
+        <h3 className='animate-pulse text-2xl md:text-4xl font-subheading text-neon-white py-2 text-center'>Portfolio Coming Soon</h3>
       </div>
     </div>
   )
